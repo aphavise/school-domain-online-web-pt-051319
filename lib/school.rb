f177f136f5783d995c677d8d18098fe76faaafe9
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-03 05:48:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+    attr_accessor :name
+
+  def initialize(school)
+    @school = school 
+    
+  end
+  
+  def roster
+    {}
+  end
+  
+  def add_student=(add_student,num)
+    @add_student = add_student
+    @num = num
+  end 
+  
+  
+
+
+
+
